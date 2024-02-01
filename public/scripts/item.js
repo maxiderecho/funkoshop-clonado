ceb42df0cc95ff757f429589ae3bbb07db235218
maxiderecho <maxi.derecho@gmail.com>
2024-02-01 15:58:09 -0300

    [FEAT] Agrego funcionalidad en los botenes + y - de la página de item y correcciones de css y html en item.ejs y shop.ejs

---
+++ public/scripts/item.js
@@ -0,0 +1,21 @@
+const itemQuantity = document.querySelector('#quantity');
+const itemAdd = document.querySelector('#add');
+const itemSubtract = document.querySelector('#subtract');
+
+itemQuantity.addEventListener('input', function () {
+    this.value = this.value.replace(/^0+/, '');
+    this.value = this.value.replace(/[^0-9]/g, '');
+    if (this.value === '') {
+        this.value = 0;
+    };
+});
+
+itemAdd.addEventListener('click', () => {
+    itemQuantity.value = Number(itemQuantity.value) + 1;
+});
+
+itemSubtract.addEventListener('click', () => {
+    if (Number(itemQuantity.value) >= 1) {
+        itemQuantity.value = Number(itemQuantity.value) - 1;
+    }
+});
